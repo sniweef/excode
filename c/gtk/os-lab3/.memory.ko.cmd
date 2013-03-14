a7ef917308aa2f5b8c1ec4525eb01e40c8a9f20e
hzhigeng <hzhigeng@gmail.com>
2013-03-14 14:55:33 +0800

    begin os-lab3

---
+++ c/gtk/os-lab3/.memory.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/sniweef/programming/c/gtk/os-lab3/memory.ko := ld -r -m elf_x86_64 -T /usr/src/linux-3.7.10-1-ARCH/scripts/module-common.lds --build-id  -o /home/sniweef/programming/c/gtk/os-lab3/memory.ko /home/sniweef/programming/c/gtk/os-lab3/memory.o /home/sniweef/programming/c/gtk/os-lab3/memory.mod.o
---
+++ c/gtk/os-lab3/.tmp_versions/memory.mod
@@ -0,0 +1,2 @@
+/home/sniweef/programming/c/gtk/os-lab3/memory.ko
+/home/sniweef/programming/c/gtk/os-lab3/memory.o
